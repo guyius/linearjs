ab0f071fa4d94eab5890efd1d3d2cb548096f93c
g.biton <g.biton@focuscura.nl>
2017-08-06 18:02:01 +0200

    feat(vector) - Added vectors basic functionality and tests.

--- src/vector.ts
+++ src/vector.ts
@@ -1,1 +1,70 @@
-export class Vector {}
+export class Vector {
+
+	add(vectorA: number[], vectorB: number[]): number[] | void {
+		if (this.validateVectorsForArithmetic(vectorA, vectorB)) {
+			return this.applyArithmeticAction('add', vectorA, vectorB);
+		}
+	}
+
+	sub(vectorA: number[], vectorB: number[]): number[] | void {
+		if (this.validateVectorsForArithmetic(vectorA, vectorB)) {
+			return this.applyArithmeticAction('subtract', vectorA, vectorB);
+		}
+	}
+
+	scale(vectorA: number[], scalar: number): number[] | void {
+		if (this.validateVectorsForScalarArithmetic(vectorA)) {
+			return this.applyArithmeticActionWithScalar('scale', vectorA, scalar);
+		}
+	}
+
+	divide(vectorA: number[], scalar: number): number[] | void {
+		if (this.validateVectorsForScalarArithmetic(vectorA)) {
+			return this.applyArithmeticActionWithScalar('divide', vectorA, scalar);
+		}
+	}
+
+	private applyArithmeticAction(action: string, vectorA: number[], vectorB: number[]): number[] {
+		let result: number[] = [];
+		vectorA.forEach((member, index) => action === 'add' ? result.push(member + vectorB[index]) : result.push(member - vectorB[index]));
+		return result;
+	}
+
+	private applyArithmeticActionWithScalar(action: string, vectorA: number[], scalar: number): number[] {
+		let result: number[] = [];
+		vectorA.forEach(member => action === 'scale' ? result.push(member * scalar) : result.push(member / scalar));
+		return result;
+	}
+
+	private validateVectorsForArithmetic(vectorA: number[], vectorB: number[]): void | boolean {
+		this.isVectorEmptyError(vectorA);
+		this.isVectorEmptyError(vectorB);
+		this.isVectorLengthEqualError(vectorA, vectorB);
+		return true;
+	}
+
+	private validateVectorsForScalarArithmetic(vectorA: number[]): void | boolean {
+		this.isVectorEmptyError(vectorA);
+		return true;
+	}
+
+	private isVectorEmptyError(vector: number[]): void {
+		if(this.isVectorEmpty(vector)) {
+			throw new Error('can\'t do arithmetic actions on empty vectors');
+		}
+	}
+
+	private isVectorLengthEqualError(vectorA: number[], vectorB: number[]): void {
+		if (!this.isVectorLengthEqual(vectorA, vectorB)) {
+			throw new Error('vectors must be of same length to do arithmetic actions');
+		}
+	}
+
+	private isVectorEmpty(vector: number[]): boolean {
+		return vector.length === 0;
+	}
+
+	private isVectorLengthEqual(vectorA: number[], vectorB: number[]): boolean {
+		return vectorA.length === vectorB.length;
+	}
+}
--- test/vector.spec.ts
+++ test/vector.spec.ts
@@ -2,7 +2,49 @@ import { Vector } from '../src/vector';
 import { expect } from 'chai';
 
 describe('Vector', () => {
-	it('expect module to exist', () => {
-		expect(Vector).to.be.a('function');
-	})
+	let vector: Vector;
+	let vectorX: number[] = [1, 2, 3];
+	let vectorY: number[] = [4, 5, 6];
+	let scalar: number = 3;
+
+	beforeEach(() => vector = new Vector());
+
+	it('expect module to exist', () => expect(vector).to.be.an('object'));
+
+	it('should add two vectors', () => {
+		const vectorSum = vector.add(vectorX, vectorY);
+		expect(vectorSum).to.deep.equal([5, 7, 9]);
+	});
+
+	it('should subtract two vectors', () => {
+		const vectorDifference = vector.sub(vectorX, vectorY);
+		expect(vectorDifference).to.deep.equal([-3, -3, -3]);
+		const oppositeVectorDifference = vector.sub(vectorY, vectorX);
+		expect(oppositeVectorDifference).to.deep.equal([3, 3, 3]);
+	});
+
+	it('should scale a vector', () => {
+		const vectorProduct = vector.scale(vectorX, scalar);
+		expect(vectorProduct).to.deep.equal([3, 6, 9]);
+	});
+
+	it('should divide a vector', () => {
+		const vectorRemainder = vector.divide(vectorX, scalar);
+		expect(vectorRemainder).to.deep.equal([1/3, 2/3, 1]);
+	});
+
+	it('should fail to do arithmetic actions on two vectors, if they have different amount of members', () => {
+		const error = 'vectors must be of same length to do arithmetic actions';
+		const fourItemVector = vectorX.concat(4);
+		expect(() => vector.add(fourItemVector, vectorY)).to.throw(error);
+		expect(() => vector.sub(fourItemVector, vectorY)).to.throw(error);
+	});
+
+	it('should fail to do arithmetic actions on two vectors, if they are empty', () => {
+		const error = 'can\'t do arithmetic actions on empty vectors';
+		expect(() => vector.add([], [])).to.throw(error);
+		expect(() => vector.sub([], [])).to.throw(error);
+		expect(() => vector.scale([], scalar)).to.throw(error);
+		expect(() => vector.divide([], scalar)).to.throw(error);
+	});
 });
